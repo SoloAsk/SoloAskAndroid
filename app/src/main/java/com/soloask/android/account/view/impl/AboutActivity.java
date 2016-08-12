6292b31d8353dea06c215691d245b51f1ce29e3a
lebron <lebron@newborn-town.com>
2016-08-12 18:40:44 +0800

    Feature: 关于页版本号修改为动态获取

--- app/src/main/java/com/soloask/android/account/view/impl/AboutActivity.java
+++ app/src/main/java/com/soloask/android/account/view/impl/AboutActivity.java
@@ -1,13 +1,21 @@
 package com.soloask.android.account.view.impl;
 
 
+import android.widget.TextView;
+
 import com.soloask.android.R;
 import com.soloask.android.common.base.BaseActivity;
+import com.soloask.android.util.Constant;
+
+import butterknife.BindView;
 
 /**
  * Created by Lebron on 2016/7/6.
  */
 public class AboutActivity extends BaseActivity {
+    @BindView(R.id.tv_version_name)
+    TextView textView;
+
     @Override
     protected int getContentViewID() {
         return R.layout.activity_about;
@@ -15,7 +23,7 @@ public class AboutActivity extends BaseActivity {
 
     @Override
     protected void initViewsAndData() {
-
+        textView.setText(Constant.getVersion(this));
     }
 
 }
